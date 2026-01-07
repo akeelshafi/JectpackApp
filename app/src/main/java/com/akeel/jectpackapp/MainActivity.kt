113117b6a79ecd2e87b53217f8907af4abc7e49e
Akeel shafi <akeelshafi20@gmail.com>
2026-01-07 18:53:36 +0530

    day 7 commit

--- app/src/main/java/com/akeel/jectpackapp/MainActivity.kt
+++ app/src/main/java/com/akeel/jectpackapp/MainActivity.kt
@@ -4,8 +4,10 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Button
@@ -14,6 +16,7 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -54,8 +57,20 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
         Button(onClick = {
             println("Button clicked!")
         }) {
-            Text(text = "Click Me")
+            Text(text = "Continue")
+        }
 
+        Image(painter = painterResource(id=R.drawable.ic_launcher_background),
+            contentDescription = "App icon")
+
+        Row(modifier = Modifier
+            .fillMaxSize()
+            .padding(16.dp)) {
+            Text(text = "Hello")
+            Text(text = "World")
+            Text(text = "World")
+            Text(text = "World")
+            Text(text = "World")
         }
     }
 
