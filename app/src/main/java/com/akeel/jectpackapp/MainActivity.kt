576d149971f6f927951a14ffe77f91318314bd3e
Akeel shafi <akeelshafi20@gmail.com>
2026-01-10 22:51:29 +0530

    day 10 commit

--- app/src/main/java/com/akeel/jectpackapp/MainActivity.kt
+++ app/src/main/java/com/akeel/jectpackapp/MainActivity.kt
@@ -107,6 +107,7 @@ fun Counter(modifier: Modifier) {
         Switch(checked = checked, onCheckedChange = { checked = it })
         Switch(checked = checked, onCheckedChange = { checked = it })
         Switch(checked = checked, onCheckedChange = { checked = it })
+        Switch(checked = checked, onCheckedChange = { checked = it })
 
 
     }
